a0d889a92012143bcc44cafe63f7a018fb4f36e9
Nalendraa <shawnaxcel42@gmail.com>
2021-03-29 16:55:22 +0700

    Create StatementDoWhile.java

---
+++ StatementDo-While/src/statementdo/pkgwhile/StatementDoWhile.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statementdo.pkgwhile;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class StatementDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n = 0;
+        
+        do {
+            
+              System.out.println("Perulangan ke-"   +   n);
+              n++;
+        } while (n <= 10);
+        
+    }
+    
+}
