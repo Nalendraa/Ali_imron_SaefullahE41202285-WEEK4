7dc96382702ac176b0754f3d5fa798df6b7c74d2
Nalendraa <shawnaxcel42@gmail.com>
2021-03-29 16:42:27 +0700

    Create StatementWhile.java

---
+++ StatementWhile/src/statementwhile/StatementWhile.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statementwhile;
+
+/**
+ *
+ * @author Ali Imron Saefullah E41202285
+ */
+public class StatementWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n = 0;
+        while (n <= 13) {
+
+            System.out.println("Perulangan ke-" + n);
+
+            // PALING PENTING!!!!! INCREMENT NILAI N
+            n++;
+        }
+
+    }
+}
