5d1d8aa1fcd829aa3e29cd72947c01f540a92bb1
Nalendraa <shawnaxcel42@gmail.com>
2021-03-31 07:22:57 +0700

    Create Tugas_B.java

---
+++ TugasWeek4/src/tugasweek4/Tugas_B.java
@@ -0,0 +1,19 @@
+
+package tugasweek4;
+
+
+public class Tugas_B {
+
+    public static void main(String[] args) {
+
+        System.out.println("            Do_While");
+        System.out.println("Bilangan kelipatan 2 (1 - 100)");
+        System.out.println("=========================");
+        int n = 1;
+        do {
+            System.out.print(n + " ");
+            n = n * 2;
+        }while (n <= 100);
+    }
+    
+}
